d0eecc4328d6bc09e1f8b1cd2911106a25931ae0
Mohanish <111619160+Mohanish7777777@users.noreply.github.com>
2023-08-06 16:30:54 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://gasofe1633:Jobijobi@1@cluster0.taf0uzx.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '-1001399998377'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'xadmin_bots'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
